02587d7ff44f043f0a4b3f78bfb2a6a5115c4c49
Amadeusz Wieczorek <git@amadeusw.com>
2019-12-05 00:12:11 -0800

    receive notification when music starts or stops

--- MediaSensor/MainWindow.xaml.cs
+++ MediaSensor/MainWindow.xaml.cs
@@ -20,9 +20,58 @@ namespace MediaSensor
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Sensor Sensor { get; }
         public MainWindow()
         {
             InitializeComponent();
+            Sensor = new Sensor();
+            ConnectUiUpdates();
+        }
+
+        private void Sensor_StateChanged(object sender, SensorStateEventArgs e)
+        {
+            // Switch to UI thread
+            this.Dispatcher.BeginInvoke((Action)(() => { this.UpdateUi(e.State); }));
+        }
+
+        private void UpdateUi(MediaState state)
+        {
+            switch (state)
+            {
+                case MediaState.Stopped:
+                    this.StatusText.Text = "Stopped";
+                    break;
+                case MediaState.Standby:
+                    this.StatusText.Text = "Standby";
+                    break;
+                case MediaState.Playing:
+                    this.StatusText.Text = "Playing";
+                    break;
+            }
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            switch (this.WindowState)
+            {
+                case WindowState.Minimized:
+                    DisconnectUiUpdates();
+                    return;
+                default:
+                    UpdateUi(Sensor.CurrentState);
+                    ConnectUiUpdates();
+                    return;
+            }
+        }
+
+        private void ConnectUiUpdates()
+        {
+            Sensor.StateChanged += Sensor_StateChanged;
+        }
+
+        private void DisconnectUiUpdates()
+        {
+            Sensor.StateChanged -= Sensor_StateChanged;
         }
     }
 }
--- MediaSensor/Sensor.cs
+++ MediaSensor/Sensor.cs
@@ -1,12 +1,120 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Timers;
 
 namespace MediaSensor
 {
-    class Sensor
+    // Thank you, Simon Mourier
+    // Source: https://stackoverflow.com/a/45483843/879243
+    internal class Sensor : IDisposable
     {
+        internal MediaState CurrentState { get; private set; }
+        internal event EventHandler<SensorStateEventArgs> StateChanged;
+        private Timer Timer { get; }
+
+        internal Sensor()
+        {
+            Timer = new Timer(500);
+            Timer.Elapsed += OnTimerElapsed;
+            Timer.Start();
+        }
+
+        public void Dispose()
+        {
+            Timer.Stop();
+            Timer.Elapsed -= OnTimerElapsed;
+        }
+
+        private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            var newState = SensorCore.GetState();
+            if (newState != CurrentState)
+            {
+                CurrentState = newState;
+                StateChanged?.Invoke(this, new SensorStateEventArgs(CurrentState));
+            }
+        }
+    }
+
+    internal enum MediaState
+    {
+        Stopped,
+        Standby,
+        Playing
+    }
+
+    internal class SensorStateEventArgs : EventArgs
+    {
+        internal MediaState State { get; }
+
+        internal SensorStateEventArgs(MediaState state)
+        {
+            this.State = state;
+        }
+    }
+
+    internal static class SensorCore
+    {
+        public static MediaState GetState()
+        {
+            var enumerator = (IMMDeviceEnumerator)(new MMDeviceEnumerator());
+            var speakers = enumerator.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eMultimedia);
+            var meter = (IAudioMeterInformation)speakers.Activate(typeof(IAudioMeterInformation).GUID, 0, IntPtr.Zero);
+            var value = meter.GetPeakValue();
+
+            // this is a bit tricky. 0 is the official "no sound" value
+            // but for example, if you open a video and plays/stops with it (w/o killing the app/window/stream),
+            // the value will not be zero, but something really small (around 1E-09)
+            // so, depending on your context, it is up to you to decide
+            // if you want to test for 0 or for a small value
+            if (value == 0)
+                return MediaState.Stopped;
+            else if (value <= 1E-08)
+                return MediaState.Standby;
+            else
+                return MediaState.Playing;
+        }
+
+        [ComImport, Guid("BCDE0395-E52F-467C-8E3D-C4579291692E")]
+        private class MMDeviceEnumerator
+        {
+        }
+
+        private enum EDataFlow
+        {
+            eRender,
+            eCapture,
+            eAll,
+        }
+
+        private enum ERole
+        {
+            eConsole,
+            eMultimedia,
+            eCommunications,
+        }
+
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown), Guid("A95664D2-9614-4F35-A746-DE8DB63617E6")]
+        private interface IMMDeviceEnumerator
+        {
+            void NotNeeded();
+            IMMDevice GetDefaultAudioEndpoint(EDataFlow dataFlow, ERole role);
+            // the rest is not defined/needed
+        }
+
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown), Guid("D666063F-1587-4E43-81F1-B948E807363F")]
+        private interface IMMDevice
+        {
+            [return: MarshalAs(UnmanagedType.IUnknown)]
+            object Activate([MarshalAs(UnmanagedType.LPStruct)] Guid iid, int dwClsCtx, IntPtr pActivationParams);
+            // the rest is not defined/needed
+        }
+
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown), Guid("C02216F6-8C67-4B5B-9D00-D008E73E0064")]
+        private interface IAudioMeterInformation
+        {
+            float GetPeakValue();
+            // the rest is not defined/needed
+        }
     }
 }
